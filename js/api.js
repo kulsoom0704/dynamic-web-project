5d73c6f67556dba77ea3b4bfd40c5f2505d2bcc0
kulsoom0704 <kulsoomhifazat2@gmail.com>
2026-08-07 14:10:47 +0200

    Fetch API data and display total records

--- js/api.js
+++ js/api.js
@@ -1,9 +1,11 @@
 const API_URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/evolution-moyenne-annuelle-demandeurs-d-emploi-inoccupes-par-commune-genre-niveau-etudes-rbc/records?limit=20";
 
-async function fetchEmploymentData() {
+export async function fetchEmploymentData() {
+
     const response = await fetch(API_URL);
+
     const data = await response.json();
 
-    console.log(data);
+    return data;
 }
-fetchEmploymentData();
+// fetchEmploymentData();
--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
+import { fetchEmploymentData } from "./api.js";
 console.log("App.js is connected successfully!");
+
 const totalRecordsElement = document.getElementById("total-records");
-totalRecordsElement.textContent = "285";
-console.log(totalRecordsElement);
+
+async function initializeApp() {
+
+    const data = await fetchEmploymentData();
+
+    totalRecordsElement.textContent = data.total_count;
+
+}
+
+initializeApp();
