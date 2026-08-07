be6f8fa38448384af9af2f99604c1ff2d7e1e981
kulsoom0704 <kulsoomhifazat2@gmail.com>
2026-08-07 13:59:09 +0200

    Connect API and update total records card

---
+++ js/api.js
@@ -0,0 +1,9 @@
+const API_URL = "https://opendata.brussels.be/api/explore/v2.1/catalog/datasets/evolution-moyenne-annuelle-demandeurs-d-emploi-inoccupes-par-commune-genre-niveau-etudes-rbc/records?limit=20";
+
+async function fetchEmploymentData() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    console.log(data);
+}
+fetchEmploymentData();
--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
 console.log("App.js is connected successfully!");
+const totalRecordsElement = document.getElementById("total-records");
+totalRecordsElement.textContent = "285";
+console.log(totalRecordsElement);
